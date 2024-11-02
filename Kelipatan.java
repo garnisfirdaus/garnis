75e030aafd794b65f75357ebb39601587c4dd520
garnisfirdaus <garnisfirdaus@gmail.com>
2024-11-02 17:35:39 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg23176016_latihan18.kelipatan;
+
+/**
+ *
+ * @author LENOVO
+ * NAMA                 : GARNIS FIRDAUS
+ * NIM                  : 23176016
+ * PRODI                : SISTEM INFORMASI 
+ * DESKRIPSI PROGRAM    : MENAMPILKAN BILANGAN KELIPATAN 3,5 SAMPAI 35
+ */
+public class Kelipatan {
+ 
+    public static void main(String[] args) {
+       double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
+ 
